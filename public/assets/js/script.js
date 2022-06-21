b9b634afdb515dbbd40f0865e393f7fd4fb59b92
beingbiplov <sharmabiplov@gmail.com>
2022-06-21 18:38:03 +0545

    add functionality to toggle top nav section

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,37 @@
 
 // set height of content section 
-const headerHeight = document.getElementsByTagName('header')[0].offsetHeight
-const contentSection = document.getElementsByClassName('content_section')[0]
+let setContentSectionHeight = () => {
+    const headerHeight = document.getElementsByTagName('header')[0].offsetHeight
+    const contentSection = document.getElementsByClassName('content_section')[0]
 
-contentSection.style.height = `calc(100% - ${headerHeight}px)`
+    contentSection.style.height = `calc(100% - ${headerHeight}px)`
+}
+
+
+
+// to toggle top nav section
+const navUpBtn = document.getElementById('nav_toggle--btn_up')
+const navDownBtn = document.getElementById('nav_toggle--btn_down')
+const top_nav = document.getElementById('top_nav')
+
+navUpBtn.addEventListener('click', () =>{
+    navUpBtn.style.display = 'none'
+    navDownBtn.style.display = 'block'
+    top_nav.style.display = 'none'
+    
+    // resize the content section 
+    setContentSectionHeight()
+})
+
+navDownBtn.addEventListener('click', () =>{
+    navDownBtn.style.display = 'none'
+    navUpBtn.style.display = 'block'
+    top_nav.style.display = 'flex'
+    
+    // resize the content section 
+    setContentSectionHeight()
+})
+
+
+// call function on window load 
+window.onload = setContentSectionHeight()
