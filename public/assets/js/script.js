3f271cf958a1339aca5b17d2951843adb84ab0ae
beingbiplov <sharmabiplov@gmail.com>
2022-06-22 21:45:22 +0545

    add remove slide function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -42,3 +42,42 @@ let resizeTextarea = (element) => {
     element.style.height = "100%";
     element.style.height = (element.scrollHeight)+"px";
 }
+
+
+// handle context menu
+// const workspace = document.getElementById('workspace')
+const cmenu = document.querySelector('.slide_context_menu')
+
+function showContextMenu(show = true){
+    if (slide1.slides.length >= 1){
+        cmenu.style.display = show ? 'block' : 'none'
+    }else{
+        cmenu.style.display = 'none'
+    }
+    
+}
+
+workspace.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    showContextMenu()
+
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left; //x position within the element.
+    let y = e.clientY - rect.top;  //y position within the element.
+
+    // to prevent menu going outside workspace 
+    topValue = (y + cmenu.offsetHeight) > workspace.offsetHeight ? (workspace.offsetHeight - cmenu.offsetHeight) : y
+    leftValue = x + cmenu.offsetWidth > workspace.offsetWidth ? window.offsetWidth - cmenu.offsetWidth : x
+    cmenu.style.top =  toPx(topValue)
+    cmenu.style.left = toPx(leftValue)
+})
+
+workspace.addEventListener('click', () =>{
+    showContextMenu(false)
+})
+
+contextMenuDel = document.getElementById('slide_context_menu--del')
+
+contextMenuDel.addEventListener('click', () =>{
+    slide1.removeSlide()
+})
--- public/assets/js/slide.js
+++ public/assets/js/slide.js
@@ -9,40 +9,43 @@ class slide{
         this.activeSlide
         this.newSLideClone
         this.previewSectionWin
+        this.newSlideDiv
+        this.newPreview
     }
 
     newSlide(){
-        this.newSlide = document.createElement('div')
-        this.newSlide.classList.add('content_section__main--slide', 'active')
-        this.activeSlide = this.newSlide
-        this.slides.push(this.newSlide)
-        this.workspace.appendChild(this.newSlide)
+        this.newSlideDiv = document.createElement('div')
+        this.newSlideDiv.classList.add('content_section__main--slide', 'active')
+        this.activeSlide = this.newSlideDiv
+        this.slides.push(this.newSlideDiv)
+        this.workspace.appendChild(this.newSlideDiv)
 
-        this.addSlideTOPreview(this.newSlide)
-        defaultLayout(this.newSlide)
+        this.addSlideTOPreview(this.newSlideDiv)
+        defaultLayout(this.newSlideDiv)
+        this.updatePreviewWindow(this.newSlideDiv)
         
     }
 
-    addSlideTOPreview(newSlide){
-        const newPreview = document.createElement('div')
-        newPreview.classList.add('slide_preview')
-        this.previewWindow.appendChild(newPreview)
+    addSlideTOPreview(newSlideDiv){
+        this.newPreview = document.createElement('div')
+        this.newPreview.classList.add('slide_preview')
+        this.previewWindow.appendChild(this.newPreview)
 
         const previewSN = document.createElement('div')
         previewSN.classList.add('preview_section__sn')
         previewSN.innerHTML = `${this.slides.length}`
-        newPreview.appendChild(previewSN)
+        this.newPreview.appendChild(previewSN)
 
         const previewBox = document.createElement('div')
         previewBox.classList.add('preview_section__box')
-        newPreview.append(previewBox)
+        this.newPreview.append(previewBox)
 
         this.previewSectionWin = document.createElement('div')
         this.previewSectionWin.classList.add('preview_section__window')
         previewBox.appendChild(this.previewSectionWin)
 
         this.previewSectionWin.innerHTML = ''
-        this.newSLideClone = newSlide.cloneNode(true)
+        this.newSLideClone = newSlideDiv.cloneNode(true)
         this.newSLideClone.classList.add('preview_section__window--slide')
         this.previewSectionWin.appendChild(this.newSLideClone)
     }
@@ -55,19 +58,47 @@ class slide{
 
     }
 
+    removePreviewWindow(){
+        this.newPreview.remove()
+    }
+
     selectLayout(layout) {
-        this.newSlide.innerHTML = ''
+        this.newSlideDiv.innerHTML = ''
         layout(this.activeSlide)
         this.updatePreviewWindow(this.activeSlide)
+    }
+
+    removeSlide(){
+        this.activeSlide.remove()
+        this.removePreviewWindow()
+        // remove form slides list 
+        const index = this.slides.indexOf(this.activeSlide);
+        if (index > -1) {
+            this.slides.splice(index, 1); 
+        }
+        if (index >= this.slides.length - 1){
+            this.activeSlide = this.slides[index-1]
+        }
+        else if (index <= 0){
+            this.activeSlide = null
+        }
+        else{
+            this.activeSlide = this.slides[index + 1]
+        }
+        console.log(this.slides)
+        if (this.slides.length <= 0){
+            slidePlaceholder.style.display = 'flex'
+        }
     }
 }
 
 
 const slide1 = new slide(workspace, previewWindow)
+let slidePlaceholder = document.getElementsByClassName('content_section__main--placeholder')[0]
 
 const newSlideBtn = document.getElementById('new_slide--btn')
 newSlideBtn.addEventListener('click', ()=>{
-    let slidePlaceholder = document.getElementsByClassName('content_section__main--placeholder')[0]
+    
     slide1.newSlide()
     slidePlaceholder.style.display = 'none'
     
